dc123b2a8820148314d1db59a3ab562288d43044
Kalyan Akella <kakella@thoughtworks.com>
2015-12-26 13:52:32 +0530

    Vertical bar chart with margins and ticks

--- bar-chart-vertical/index.js
+++ bar-chart-vertical/index.js
@@ -1,31 +1,49 @@
 
-var width = 960, height = 500;
+var margin = {top: 20, right: 30, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
 var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
-var y = d3.scale.linear().range([height, 0]);
+var xAxis = d3.svg.axis().scale(x).orient("bottom");
 
-var chart = d3.select(".chart").attr("width", width).attr("height", height);
+var y = d3.scale.linear().range([height, 0]);
+var yAxis = d3.svg.axis().scale(y).orient("left").ticks(10, "%");
+
+var chart = d3.select(".chart")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+              .append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var type = function(d) {
-  d.frequency = +d.frequency;
+  d.value = +d.value;
   return d;
 }
 
 d3.csv("data.csv", type, function(error, data) {
-  x.domain(data.map(function(d) { return d.letter; }));
-  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+  x.domain(data.map(function(d) { return d.name; }));
+  y.domain([0, d3.max(data, function(d) { return d.value; })]);
   
-  var bar = chart.selectAll("g").data(data).enter().append("g")
-    .attr("transform", function(d, i) { return "translate(" + x(d.letter) + ", 0)"; });
+  chart.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
 
-  bar.append("rect")
-      .attr("y", function(d) { return y(d.frequency); })
-      .attr("height", function(d) { return height - y(d.frequency); })
+  chart.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Frequency");
+
+  chart.selectAll(".bar")
+      .data(data)
+      .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.name); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return height - y(d.value); })
       .attr("width", x.rangeBand());
-
-  bar.append("text")
-      .attr("x", x.rangeBand() / 2)
-      .attr("y", function(d) { return y(d.frequency) + 3; })
-      .attr("dy", ".75em")
-      .text(function(d) { return d.letter; });
 });
